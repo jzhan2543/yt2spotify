eb129cb57581bb28bf450a333e884b00b8680b10
Jeff Zhan <jeffzhan2008@gmail.com>
2020-04-22 15:42:38 -0400

    spotify URI

--- yt2spotify/yt2spotify/createplaylist.py
+++ yt2spotify/yt2spotify/createplaylist.py
@@ -1,14 +1,21 @@
 import json 
 import os 
 import requests
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
 from datetime import date 
 
-from secrets import user_id, token 
+from secrets import spotify_user_id, spotify_token_1, spotify_token_2
 
 class CreatePlaylist: 
 
   def __init__(self): 
-    pass 
+    self.user_id = spotify_user_id
+    self.token = spotify_token_1
+    self.token_2 = spotify_token_2 
+ 
 
   def getYT(self): 
     pass 
@@ -25,20 +32,34 @@ class CreatePlaylist:
       "public": False
     })
 
-    q = "https://api.spotify.com/v1/users/{}/playlists".format(user_id) 
+    q = "https://api.spotify.com/v1/users/{}/playlists".format(self.user_id) 
     r = requests.post(
       q, 
       data = json_body, 
-      headers={
+      headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer {}".format(token)
+        "Authorization": "Bearer {}".format(self.token_2)
       }
     )
     response_json = r.json()
     return response_json["id"]
 
-  def getSpotifyURI(self): 
-    pass 
+  def getSpotifyURI(self, song_name, artist): 
+    q =  "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
+      song_name,
+      artist
+    )
+    r = requests.get(
+      q, 
+      headers = { 
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {}".format(self.token_2)
+      }
+    )
+    response_json = r.json() 
+    songs = response_json["tracks"]["items"]
+
+    uri = songs[0]["uri"]
 
   def addSong(self): 
     pass
@@ -55,16 +76,39 @@ if __name__ == '__main__':
     #   "public": False
     # })
 
-    # q = "https://api.spotify.com/v1/users/{}/playlists".format(user_id) 
+    # q = "https://api.spotify.com/v1/users/{}/playlists".format(spotify_user_id) 
     # r = requests.post(
     #   q, 
     #   data = json_body, 
     #   headers={
     #     "Content-Type": "application/json",
-    #     "Authorization": "Bearer {}".format(token)
+    #     "Authorization": "Bearer {}".format(spotify_token_2)
     #   }
     # )
     # response_json = r.json()
+    # print(response_json)
+    # print("--------")
+    # song_name = "Suge"
+    # artist = "DaBaby"
+
+    # q =  "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
+    #   song_name,
+    #   artist
+    # )
+    # r = requests.get(
+    #   q, 
+    #   headers = { 
+    #     "Content-Type": "application/json",
+    #     "Authorization": "Bearer {}".format(spotify_token_2)
+    #   }
+    # )
+    # response_json = r.json() 
+
+    # songs = response_json["tracks"]["items"]
+
+    # uri = songs[0]["uri"]
+    # print(uri)
+
 
 
     
